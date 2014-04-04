63309fb120b7887410300aff06fd055fd0066d14
John Traver <john@aerospike.com>
2014-04-04 12:55:33 -0700

    updates

--- python/json/json1.py
+++ python/json/json1.py
@@ -23,3 +23,16 @@ fileh = open(filename, "r")
 json1 = json.load(fileh)
 fileh.close()
 print "json1 = %s" % str(json1)
+
+hosts = ['192.168.75.205', '192.168.75.206']
+filename = "hosts.json"
+fileh = open(filename, "w")
+output = json.dumps(hosts)
+fileh.write(output)
+fileh.close()
+fileh = open(filename, "r")
+input = fileh.read()
+fileh.close()
+json1 = json.loads(input)
+print json1[0]
+print json1[1]
