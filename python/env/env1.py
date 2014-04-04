3cc981171618a73f3c3ad7a6efbe9c8c54a30943
John Traver <john@aerospike.com>
2014-04-04 11:44:53 -0700

    updates

--- python/env/env1.py
+++ python/env/env1.py
@@ -1,7 +1,10 @@
 import os
+import sys
 
 print "env = %s" % str(os.environ)
 
 print os.environ['HOME']
 print os.environ.get('HOME')
 print os.environ.get('JOB_NAME', 'job_name')
+
+print sys.prefix
