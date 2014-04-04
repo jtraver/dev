5ccafd44377e78805c47b688d7c57e7b9a43cb01
John Traver <john@aerospike.com>
2014-04-04 11:30:05 -0700

    updates

--- python/pid/pid1.py
+++ python/pid/pid1.py
@@ -1,3 +1,19 @@
 import os
 
-print "pid = %s" % str(os.getpid())
+pid1 = str(os.getpid())
+print "pid1 = %s" % pid1
+
+filename = "/tmp/" + pid1
+print "filename = %s" % filename
+
+fileh = open(filename, "w");
+fileh.write(pid1)
+fileh.close()
+
+fileh = open(filename, "r");
+pid2 = fileh.read()
+
+if pid1 == pid2:
+    print "success"
+else:
+    print "failure"
