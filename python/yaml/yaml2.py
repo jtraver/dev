f8b88d78e2c3c2fadf4858bc137dcf0ee7d0e959
John Traver <john@aerospike.com>
2014-07-22 18:36:26 -0700

    repo 3

--- python/yaml/yaml2.py
+++ python/yaml/yaml2.py
@@ -8,7 +8,7 @@ print "yaml = %s" % dir(yaml)
 # http://stackoverflow.com/questions/12470665/yaml-writing-data-into-yaml-through-python
 
 config1 = {
-    'repos2': [
+    'repos3': [
         {
             'name': 'aerospike-client-c',
             'owner': 'citrusleaf',
