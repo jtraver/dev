9fc602c14887bda008b3f50d33de9c0de65e6212
John Traver <john@aerospike.com>
2014-08-20 09:17:04 -0700

    updates

---
+++ python/re/mod1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import re
+
+print "re = %s" % str(re)
+print "re = %s" % dir(re)
+
+# re = ['DEBUG', 'DOTALL', 'I', 'IGNORECASE', 'L', 'LOCALE', 'M', 'MULTILINE', 'S', 'Scanner', 'T', 'TEMPLATE', 'U', 'UNICODE', 'VERBOSE', 'X', '_MAXCACHE', '__all__', '__builtins__', '__doc__', '__file__', '__name__', '__package__', '__version__', '_alphanum', '_cache', '_cache_repl', '_compile', '_compile_repl', '_expand', '_pattern_type', '_pickle', '_subx', 'compile', 'copy_reg', 'error', 'escape', 'findall', 'finditer', 'match', 'purge', 'search', 'split', 'sre_compile', 'sre_parse', 'sub', 'subn', 'sys', 'template']
