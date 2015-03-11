06ba15f1ff22dae8ddbca24b8d514359652c974d
John Traver <john@aerospike.com>
2015-03-11 20:15:52 +0000

    string seems to fix it

--- python/subprocess/tar1.py
+++ python/subprocess/tar1.py
@@ -8,7 +8,9 @@ import shutil
 
 if os.path.exists('envars.prop'):
     shutil.copy2('envars.prop', "work.current")
-args = ('tar', '-czvf', 'work.tgz', ".")
+# args = ('tar', '-czvf', 'work.tgz', ".")
+# args = ['tar', '-czvf', 'work.tgz', "."]
+args = "tar -czvf work.tgz ."
 popen = subprocess.Popen(
     args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='.', shell=True)
 out, err = popen.communicate()
