1ba9ffc07dcd22796bc43f62102783ea1de2f970
John Traver <john@aerospike.com>
2015-06-09 11:30:08 -0700

    updates

--- python/help/aerospike1.py
+++ python/help/aerospike1.py
@@ -59,18 +59,20 @@ def info2(object):
             pass
 
 # info(aerospike)
-info2(aerospike)
 
-print "\n"
-count = 0
-for k, e in found.items():
-    if callable(e):
-        count += 1
-        print "%s %s" % (str(count), k)
-        try:
-            e()
-        except Exception, ex:
-            print "    %s" % str(ex)
+def do_info2():
+    info2(aerospike)
+
+    print "\n"
+    count = 0
+    for k, e in found.items():
+        if callable(e):
+            count += 1
+            print "%s %s" % (str(count), k)
+            try:
+                e()
+            except Exception, ex:
+                print "    %s" % str(ex)
 
 def pinfos():
     pinfo(aerospike)
@@ -89,3 +91,65 @@ def pinfos():
     pinfo(aerospike.set_serializer)
 
 # pinfos()
+
+
+def dir1(object):
+    print "\n"
+    print "object = %s" % dir(object)
+    for e in dir(object):
+        if '__' in e:
+            continue
+        attr = getattr(object, e)
+        # if 'builtin_function_or_method' in str(type(attr)) or type(attr) == int or type(attr) == str or 'NoneType' in str(type(attr)):
+        if type(attr) == int or type(attr) == str or 'NoneType' in str(type(attr)):
+            continue
+        print "e = %s" % str(e)
+        # e is just a string
+        # print "  type e = %s" % type(e)
+        # print "  dir e = %s" % dir(e)
+        print "  type attr = %s" % type(attr)
+        # print "    dir attr = %s" % dir(attr)
+
+
+def dir2(object):
+    attrs = []
+    for e in dir(object):
+        if '__' in e:
+            continue
+        attr = getattr(object, e)
+        # if 'builtin_function_or_method' in str(type(attr)) or type(attr) == int or type(attr) == str or 'NoneType' in str(type(attr)):
+        if type(attr) == int or type(attr) == str or 'NoneType' in str(type(attr)):
+            continue
+        attrs.append(e)
+    return attrs
+
+def main():
+    # dir1(aerospike)
+    attrs = dir2(aerospike)
+    print "attrs = %s" % str(attrs)
+    for e in attrs:
+        print "\naerospike.%s" % e
+        attr = getattr(aerospike, e)
+        if 'method' in str(type(attr)):
+            continue
+        # dir1(attr)
+        attrs2 = dir2(attr)
+        print "  attrs2 = %s" % str(attrs2)
+        for e2 in attrs2:
+            print "\naerospike.%s.%s" % (e, e2)
+            attr3 = getattr(attr, e2)
+            if 'method' in str(type(attr3)):
+                continue
+            # dir1(attr3)
+            attrs3 = dir2(attr3)
+            print "  attrs3 = %s" % str(attrs3)
+            for e3 in attrs3:
+                print "\naerospike.%s.%s.%s" % (e, e2, e3)
+                attr4 = getattr(attr3, e3)
+                if 'method' in str(type(attr4)):
+                    continue
+                # dir1(attr4)
+                attrs4 = dir2(attr4)
+                print "  attrs4 = %s" % str(attrs4)
+
+main()
