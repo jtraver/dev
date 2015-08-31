d7aeb0fb89a4a6ddc2d62f393a3806a001f7a449
John Traver <john@aerospike.com>
2015-08-31 12:58:12 -0700

    if syntax

---
+++ python/if/if1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+str1 = 'test1'
+
+if str1 == 'test1' or str1 == 'test2':
+    print '1 or 2'
+elif str1 == 'test3' or str1 == 'test4':
+    print "3 or 4"
+else:
+    print "else"
