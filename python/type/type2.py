301d14c0de5bb90312a75d5f565714384752cfbc
John Traver <john@aerospike.com>
2015-12-11 12:26:00 -0800

    updates

---
+++ python/type/type2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+
+def get_value(s):
+    try:
+        return int(s)
+    except ValueError:
+        pass
+    try:
+        return float(s)
+    except ValueError:
+        pass
+    return s
+
+def main():
+    str1 = '1'
+    val1 = get_value(str1)
+    print "val1 = %s" % str(val1)
+    print "val1 = %s" % str(type(val1))
+    str1 = '1.2'
+    val1 = get_value(str1)
+    print "val1 = %s" % str(val1)
+    print "val1 = %s" % str(type(val1))
+    float1 = 1.211111111111111111111111111111111111111111111111111111111111111111111111111111111
+    print "float1 = %s" % str(float1)
+    print "float1 = %s" % str(type(float1))
+    str1 = '1.211111111111111111111111111111111111111111111111111111111111111111111111111111111'
+    val1 = get_value(str1)
+    print "val1 = %s" % str(val1)
+    print "val1 = %s" % str(type(val1))
+    str1 = 'a string'
+    val1 = get_value(str1)
+    print "val1 = %s" % str(val1)
+    print "val1 = %s" % str(type(val1))
+
+
+main()
