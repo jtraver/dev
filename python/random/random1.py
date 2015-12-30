8f9d3592b4d8e230f9e9b27e4459f2376cbde8e9
John Traver <john@aerospike.com>
2015-12-30 08:04:22 -0800

    updates

---
+++ python/random/random1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import random
+import apihelper
+
+# len1 = random.randint(0, max_byte_array)
+# byte = random.randint(0, 255)
+# nbins = random.randint(0, max_list)
+# dtype = random.randint(0, 2)
+# htype = random.randint(0, len(basic) - 1)
+# dtype = random.randint(0, len(hashable) - 1)
+# dtype = random.randint(0, 1)
+# val = random.random() * random.randint(1, 100000)
+# val = random.randint(1, 100000)
+# nbins = random.randint(0, max_list)
+# dtype = random.randint(0, len(types) - 1)
+# nbins = random.randint(0, max_dict)
+# dtype = random.randint(0, len(basic) - 1)
+# nbins = random.randint(0, max_string)
+# random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# ))
+# nbins = random.randint(0, max_unicode)
+# u = unichr(random.randint(0, max_unicode_char))
+# c = random.randint(0, 1)
+# u = u + unichr(random.randint(0, max_unicode_char))
+# u = u + unichr(random.randint(0xd800, 0xdbff))
+# u = u + unichr(random.randint(0xdc00, 0xdfff))
+# dtype = random.randint(0, 1)
+# val = random.random() * random.randint(1, 100000)
+# val = random.randint(1, 100000)
+# btype = random.randint(0, 32)
+# nbins = random.randint(0, tot_bins)
+# dtype = random.randint(0, 2)
+# htype = random.randint(0, len(basic) - 1)
+
+print "random = %s" % str(random)
+print "random = %s" % str(dir(random))
+# apihelper.info(object)
