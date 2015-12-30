38450ad3ef45a3eb8096a61c0fd5db12b23b0d06
John Traver <john@aerospike.com>
2015-12-30 08:40:48 -0800

    updates

--- python/500/500a.py
+++ python/500/500a.py
@@ -11,11 +11,45 @@ def create_deck():
         deck.append('K' + suit)
         for card in xrange(4, 11):
             deck.append(str(card) + suit)
-    deck.append('J')
+    deck.append('JOKER')
     return deck
+
+def deal_card(deck):
+    # print "deck = %s" % str(deck)
+    index = random.randint(0, len(deck) - 1)
+    # print "index = %s" % str(index)
+    card = deck[index]
+    while card == None:
+        index = random.randint(0, len(deck) - 1)
+        # print "index = %s" % str(index)
+        card = deck[index]
+    deck[index] = None
+    return card
+
+def deal(deck):
+    hands = []
+    for index in xrange(0, 5):
+        # print "index = %s" % str(index)
+        hands.append([])
+    for index in xrange(0, len(hands)):
+        for card in xrange(0, 3):
+            hands[index].append(deal_card(deck))
+    for index in xrange(0, len(hands)):
+        for card in xrange(0, 2):
+            hands[index].append(deal_card(deck))
+    for index in xrange(0, len(hands) - 1):
+        for card in xrange(0, 3):
+            hands[index].append(deal_card(deck))
+    for index in xrange(0, len(hands) - 1):
+        for card in xrange(0, 2):
+            hands[index].append(deal_card(deck))
+    # print "hands = %s" % str(hands)
+    return hands
 
 def main():
     deck = create_deck()
-    print "deck = %s" % str(deck)
+    # print "deck = %s" % str(deck)
+    hands = deal(deck)
+    print "hands = %s" % str(hands)
 
 main()
