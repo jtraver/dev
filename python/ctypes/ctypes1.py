be292a5d1edc34fa065408feb9200c38590c6988
John Traver <john@aerospike.com>
2016-01-26 11:41:12 -0800

    updates

--- python/ctypes/ctypes1.py
+++ python/ctypes/ctypes1.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
-from ctypes import create_string_buffer
+import ctypes
+# from ctypes import create_string_buffer
+import apihelper
 
-buf = create_string_buffer(10)
+buf = ctypes.create_string_buffer(10)
 print "buf = %s" % str(buf)
 print "buf = %s" % str(type(buf))
 print "buf = %s" % str(dir(buf))
@@ -10,3 +12,5 @@ buf[0] = 'a'
 print "buf = %s" % str(buf.raw)
 print "buf = %s" % str(buf[0])
 print "buf = %x" % ord(buf[0])
+
+apihelper.info(ctypes)
