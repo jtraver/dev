a9e670dbd0a5c1242586bce39fd7db83172f94e5
John Traver <john@aerospike.com>
2016-05-27 13:32:11 -0600

    updates

--- python/sys/sys1.py
+++ python/sys/sys1.py
@@ -5,3 +5,24 @@ import sys
 print "sys = %s" % str(sys)
 print "sys = %s" % str(dir(sys))
 print "sys.platform = %s" % str(sys.platform)
+
+
+def func1():
+    print sys._getframe().f_code.co_name
+
+
+def func2():
+    print sys._getframe().f_code.co_name
+
+
+def func3():
+    print sys._getframe().f_code.co_name
+
+
+def main():
+    func1()
+    func2()
+    func3()
+
+
+main()
