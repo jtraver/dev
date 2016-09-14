745346800c60e67fab9c002c6a6ffdbe5db8d5f6
John Traver <john@aerospike.com>
2016-09-14 16:29:56 -0400

    updates

--- java/maven1/hello1.sh
+++ java/maven1/hello1.sh
@@ -1,4 +1,17 @@
 # https://maven.apache.org/guides/getting-started/maven-in-five-minutes.html
+
+echo CLASSPATH = $CLASSPATH
+# export CLASSPATH=.;$CLASSPATH
+# export CLASSPATH=.
+export CLASSPATH=/home/jtraver/dev/git/jtraver/dev/java/maven1/my-app/target/classes
+# ./my-app/target/classes/com/mycompany/app/App.class
+echo CLASSPATH = $CLASSPATH
+
+rm -rf my-app
 mvn --version
 mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
 find my-app
+cd my-app
+mvn package
+cd ..
+java com.mycompany.app.App
