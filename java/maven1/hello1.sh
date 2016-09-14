aba4a9d1650e7babfbb82270d52ee6ffaa27933f
John Traver <john@aerospike.com>
2016-09-14 16:22:01 -0400

    updates

---
+++ java/maven1/hello1.sh
@@ -0,0 +1,4 @@
+# https://maven.apache.org/guides/getting-started/maven-in-five-minutes.html
+mvn --version
+mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+find my-app
