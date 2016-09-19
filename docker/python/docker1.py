ec288b9d829545454cb0dcc002c66d5c7cc396f1
John Traver <john@aerospike.com>
2016-09-19 17:15:55 -0400

    updates

--- docker/python/docker1.py
+++ docker/python/docker1.py
@@ -2,8 +2,42 @@
 
 import apihelper
 import docker
-import env as env1
+# import env as env1
 import os
+import io
+import tarfile
+
+def put_file(container, filepath, content):
+    """Upload a file to a container."""
+    return put_files(container, [(filepath, content)])
+
+def put_files(container, files):
+    """Upload files to a container.
+    The files parameter is a list of tuples of either:
+      - (path, content, mode)
+      - (path, content)
+    """
+    ts = io.BytesIO()
+    t = tarfile.TarFile(mode='w', fileobj=ts)
+    for ft in files:
+        fp = None
+        fc = None
+        fm = None
+        if len(ft) == 3:
+            fp, fc, fm = ft
+        else:
+            fp, fc = ft
+        fs = io.BytesIO()
+        fs.write(fc)
+        fs.seek(0)
+        fi = tarfile.TarInfo(name=fp)
+        fi.size = len(fs.getvalue())
+        if fm:
+            fi.mode = fm
+        t.addfile(tarinfo=fi, fileobj=fs)
+        t.close()
+    ts.seek(0)
+    return dockerClient.put_archive(container=container, path="/", data=ts)
 
 
 def pinfo(object):
@@ -15,6 +49,7 @@ def pinfo(object):
 
 
 def main():
+    global dockerClient
     pinfo(docker)
     dockerClient = docker.Client(base_url='unix://var/run/docker.sock')
     pinfo(dockerClient)
@@ -47,14 +82,17 @@ def main():
             e = dockerClient.exec_create(container=container, cmd="ls")
             start1 = dockerClient.exec_start(exec_id=e['Id'])
             print "start1 = %s" % str(start1)
-    env = os.environ
-    print
-    print "env = %s" % str(env)
-    pinfo(env)
-    # print "env = %s" % str(env.map())
-    PROJECT = env.get("PROJECT_ID")
-    # PROJECT = env.map("PROJECT_ID")
-    # PROJECT = env.map()
-    print "PROJECT = %s" % str(PROJECT)
+            # dockerClient.put_archive(container=container, path="/tmp", data="./archive1.tar")
+            put_file(container, 'tmp.tmp', "test tmp.tmp")
+    if False:
+        env = os.environ
+        print
+        print "env = %s" % str(env)
+        pinfo(env)
+        # print "env = %s" % str(env.map())
+        PROJECT = env.get("PROJECT_ID")
+        # PROJECT = env.map("PROJECT_ID")
+        # PROJECT = env.map()
+        print "PROJECT = %s" % str(PROJECT)
 
 main()
