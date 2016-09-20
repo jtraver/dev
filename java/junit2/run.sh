4fa5553290cd3ceed575aa3b70d04652dbf58e89
John Traver <john@aerospike.com>
2016-09-20 16:25:09 -0400

    updates

--- java/junit2/run.sh
+++ java/junit2/run.sh
@@ -1,3 +1,5 @@
 javac Calculator.java
 javac -cp .:/usr/share/java/junit.jar CalculatorTest.java
 # /usr/share/java/junit.jar
+java -cp .:/usr/share/java/junit.jar:/usr/share/java/hamcrest/core.jar org.junit.runner.JUnitCore CalculatorTest
+
