0e2e925c55220996d94ec7eb897ee4a2ca23e2aa
John Traver <john@aerospike.com>
2016-09-20 14:33:53 -0400

    updates

---
+++ java/maven2/hello1.sh
@@ -0,0 +1,32 @@
+# https://maven.apache.org/guides/getting-started/maven-in-five-minutes.html
+
+echo CLASSPATH = $CLASSPATH
+# export CLASSPATH=.;$CLASSPATH
+# export CLASSPATH=.
+export CLASSPATH=/home/jtraver/dev/git/jtraver/dev/java/maven2/my-app/target/classes
+# ./my-app/target/classes/com/mycompany/app/App.class
+echo CLASSPATH = $CLASSPATH
+
+# rm -rf my-app
+mvn --version
+# echo mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+# mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+# echo
+# echo generated my-app skeleton:
+find my-app
+cd my-app
+echo mvn package
+mvn package
+echo
+echo after mvn package
+find .
+echo mvn site
+mvn site
+echo
+echo after mvn site
+find .
+cd ..
+echo
+java com.mycompany.app.App
+# export CLASSPATH=
+java -cp my-app/target/my-app-1.0-SNAPSHOT.jar com.mycompany.app.App
