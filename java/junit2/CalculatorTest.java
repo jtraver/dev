991f291108ea4830ee3a4cdc57cb83897863a3bd
John Traver <john@aerospike.com>
2016-09-27 17:54:17 -0400

    updates

---
+++ java/junit2/CalculatorTest.java
@@ -0,0 +1,13 @@
+// https://github.com/junit-team/junit4/wiki/Getting-started
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+  @Test
+  public void evaluatesExpression() {
+    Calculator calculator = new Calculator();
+    int sum = calculator.evaluate("1+2+3");
+    assertEquals(6, sum);
+  }
+}
