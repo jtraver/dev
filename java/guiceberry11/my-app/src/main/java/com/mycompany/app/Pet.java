3e6c98b459f2ae629ea3e057a158f136c364c975
John Traver <john@aerospike.com>
2016-09-27 17:49:08 -0400

    updates

---
+++ java/guiceberry11/my-app/src/main/java/com/mycompany/app/Pet.java
@@ -0,0 +1,19 @@
+// package tutorial_1_server.prod;
+package com.mycompany.app;
+
+/**
+ * Enumeration of all pets for sale in our tutorial Pet Store.
+ */
+public enum Pet {
+  CHAMELEON,
+  CAPYBARA,
+  CAT,
+  DOG,
+  FERRET,
+  HUMMINGBIRD,
+  MACAW,
+  PARROT,
+  TAMARIN,
+  TOUCAN,
+  WEASEL,
+}
