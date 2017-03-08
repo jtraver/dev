0e012adaa148642d895f35ac22cb44d519f1f7e6
John Traver <john@aerospike.com>
2017-03-08 14:12:51 -0500

    updates

---
+++ python/deps/deps1.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python
+
+import apihelper
+import sys
+import os
+import platform
+
+
+has_argparse = False
+try:
+    import argparse
+    has_argparse = True
+except Exception, e:
+    print "e = %s" % str(e)
+if has_argparse:
+    apihelper.info(argparse)
+
+
+has_bcrypt = False
+try:
+    import bcrypt
+    has_bcrypt = True
+except Exception, e:
+    print "e = %s" % str(e)
+if has_bcrypt:
+    apihelper.info(bcrypt)
+
+
+has_openssl = False
+try:
+    import OpenSSL
+    has_openssl = True
+except Exception, e:
+    print "e = %s" % str(e)
+if has_openssl:
+    apihelper.info(OpenSSL)
+
+has_pexpect = False
+try:
+    import pexpect
+    has_pexpect = True
+except Exception, e:
+    print "e = %s" % str(e)
+if has_pexpect:
+    apihelper.info(pexpect)
+
+has_ply = False
+try:
+    import ply
+    has_ply = True
+except Exception, e:
+    print "e = %s" % str(e)
+if has_ply:
+    apihelper.info(ply)
+
+
+
+print "platform = %s" % str(sys.platform)
+print "os name = %s" % str(os.name)
+# apihelper.info(sys.platform)
+# apihelper.info(os.name)
+os.system("uname -a")
+apihelper.info(platform)
+
+print "platform.platform = %s" % str(platform.platform())
+print "platform.system = %s" % str(platform.system())
+print "platform.release = %s" % str(platform.release())
+print "platform.version = %s" % str(platform.version())
+print "platform.linux_distrubution = %s" % str(platform.linux_distribution())
+
+print "sys.version = %s" % str(sys.version)
+print "platform.dist = %s" % str(platform.dist())
+print "platform.system = %s" % str(platform.system())
+print "platform.machine = %s" % str(platform.machine())
+print "platform.platform = %s" % str(platform.platform())
+print "platform.uname = %s" % str(platform.uname())
+print "platform.version = %s" % str(platform.version())
+print "platform.mac_ver = %s" % str(platform.mac_ver())
+
+osname = "centos"
+print "\nchecking pip for argparse"
+os.system("pip list --format=columns | grep -i argparse")
+print "\nchecking pip for bcrypt"
+os.system("pip list --format=columns | grep -i bcrypt")
+print "\nchecking pip for openssl"
+os.system("pip list --format=columns | grep -i openssl")
+print "\nchecking pip for pexpect"
+os.system("pip list --format=columns | grep -i pexpect")
+print "\nchecking pip for ply"
+os.system("pip list --format=columns | grep -i ply")
+if "centos" in platform.dist():
+    print "found centos"
+    print "\nchecking rpm for argparse"
+    os.system("rpm -q -a | grep -i argparse")
+    print "\nchecking rpm for bcrypt"
+    os.system("rpm -q -a | grep -i bcrypt")
+    print "\nchecking rpm for openssl"
+    os.system("rpm -q -a | grep -i openssl")
+    print "\nchecking rpm for pexpect"
+    os.system("rpm -q -a | grep -i pexpect")
+    print "\nchecking rpm for ply"
+    os.system("rpm -q -a | grep -i ply")
+else:
+    osname = "debian"
+    print "\nchecking dpkg for argparse"
+    os.system("dpkg -l | grep -i argparse")
+    print "\nchecking dpkg for bcrypt"
+    os.system("dpkg -l | grep -i bcrypt")
+    print "\nchecking dpkg for openssl"
+    os.system("dpkg -l | grep -i openssl")
+    print "\nchecking dpkg for pexpect"
+    os.system("dpkg -l | grep -i pexpect")
+    print "\nchecking dpkg for ply"
+    os.system("dpkg -l | grep -i ply")
+
+if has_argparse:
+    print "has argparse"
+else:
+    print "does not have argparse"
+
+if has_bcrypt:
+    print "has bcrypt"
+else:
+    print "does not have bcrypt"
+
+if has_openssl:
+    print "has openssl"
+else:
+    print "does not have openssl"
+
+if has_pexpect:
+    print "has pexpect"
+else:
+    print "does not have pexpect"
+
+if has_ply:
+    print "has ply"
+else:
+    print "does not have ply"
