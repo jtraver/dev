f5a2d2b0395db9951c69958977a152a2875c4c25
John Traver <john@aerospike.com>
2018-01-16 12:18:22 -0700

    updates

--- python/option/option1.py
+++ python/option/option1.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
 
 import optparse
+
+def main():
+    optparser = optparse.OptionParser()
+    optparser.add_option("--verbose", action="store_true", default=False)
+    (options, args) = optparser.parse_args()
+    print "options = %s" % str(options)
+    print "args = %s" % str(args)
+
+main()
