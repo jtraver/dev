2f4bd8cf61a139bff514675050d0f0a3a72653e6
John Traver <john@aerospike.com>
2018-08-28 16:41:20 -0600

    commit_mac_auto_update.180828164120

---
+++ python/html/html1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import urllib2
+from HTMLParser import HTMLParser
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print "Encountered a start tag:", tag
+
+    def handle_endtag(self, tag):
+        print "Encountered an end tag :", tag
+
+    def handle_data(self, data):
+        print "Encountered some data  :", data
+
+def main():
+    url = 'https://screener.finance.yahoo.com/stocks.html'
+    # url = 'https://screener.finance.yahoo.com/b?sc=&im=&prmin=0&prmax=&mcmin=&mcmax=&dvymin=0&dvymax=&betamin=&betamax=&remin=&remax=&pmmin=&pmmax=&pemin=&pemax=&pbmin=&pbmax=&psmin=&psmax=&pegmin=&pegmax=&gr=&grfy=&ar=&vw=1&db=stocks'
+    req = urllib2.Request(url)
+    resp = urllib2.urlopen(req)
+    html = resp.read()
+    # print "%s" % resp.read()
+    parser = MyHTMLParser()
+    parser.feed(html)
+
+main()
