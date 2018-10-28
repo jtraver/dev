e80b39135316c10838ca4e22ecb7307d69c5227b
John Traver <john@aerospike.com>
2018-10-28 04:17:58 -0600

    updaes

--- scheme/hello/helloworld.sh
+++
@@ -1,2 +0,0 @@
-# scheme --load helloworld.scm --batch-mode
-scheme --batch-mode --quiet --silent --load helloworld.scm
---
+++ scheme/hello/run.sh
@@ -0,0 +1,4 @@
+# scheme --load helloworld.scm --batch-mode
+# scheme --interactive --batch-mode --quiet --silent --load helloworld.scm > h.out 2>&1
+# vi h.out
+scheme --interactive --batch-mode --quiet --silent --load helloworld.scm
