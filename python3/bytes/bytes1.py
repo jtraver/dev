5f9a99b4df6d1cb39dc3920e67d3cf7f3ce5cb60
John Traver <john@aeropsike.com>
2019-10-31 14:49:53 -0600

    updates

--- python3/bytes/bytes1.py
+++ python3/bytes/bytes1.py
@@ -5,4 +5,15 @@ print("str = %s" % str(str))
 print("str = %s" % str(dir(str)))
 
 str1 = "bytes"
-print("str1 = %s" % str1.encode())
+print("str1 = %s" % str1)
+str2 = str1.encode()
+print("str2 = %s" % str2)
+str3 = str2.decode()
+print("str3 = %s" % str3)
+
+if str1 == str3:
+    print("coverted ok")
+else:
+    print("FAIL did not covert ok")
+    print("FAIL str1 = %s" % str1)
+    print("FAIL str3 = %s" % str3)
