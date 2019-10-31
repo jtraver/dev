606a276908b6bd9a1cab44b8fbf89fee29613395
John Traver <john@aeropsike.com>
2019-10-30 19:44:03 -0600

    updates

---
+++ python/littleendian/le1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+# 61196 versus 3311
+
+int0 = 3311
+print "%x" % int0
+print "%d" % int0
+
+int1 = int("cef", 16)
+print "%x" % int1
+print "%d" % int1
+
+int2 = int("fec", 16)
+print "%x" % int2
+print "%d" % int2
+
+int3 = int("ef0c", 16)
+print "%x" % int3
+print "%d" % int3
+
+int4 = 61196 - 61196
+print "%x" % int4
+print "%d" % int4
+
+print " "
+# 9477 versus 1317
+
+int0 = 1317
+print "%x" % int0
+print "%d" % int0
+
+int1 = int("525", 16)
+print "%x" % int1
+print "%d" % int1
+
+int2 = int("2505", 16)
+print "%x" % int2
+print "%d" % int2
+
+int3 = int("2505", 16)
+print "%x" % int3
+print "%d" % int3
+
+int4 = 9477 - 9477
+print "%x" % int4
+print "%d" % int4
