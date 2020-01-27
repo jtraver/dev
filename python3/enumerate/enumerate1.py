3d1c5563f4533d3b3c6dbca1868e2e453675a626
John Traver <john@aeropsike.com>
2020-01-27 09:07:40 -0700

    commit_dev_auto_update.200127090740

---
+++ python3/enumerate/enumerate1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+
+VT100_BOLD = "[0;1m"
+VT100_RED = "[0;1;31m"
+VT100_GREEN = "[0;1;32m"
+VT100_STOP_MARKUP = "[0m"
+
+def main():
+    ret1 = 0
+    ret1 += enumerate1()
+    if ret1:
+        print("%sFAIL %s%s" % (VT100_RED, str(ret1), VT100_STOP_MARKUP))
+    else:
+        print("%sPASS%s" % (VT100_GREEN, VT100_STOP_MARKUP))
+    print("DONE")
+    print("")
+    sys.stdout.flush()
+    sys.exit(ret1)
+
+def enumerate1():
+    ret1 = 0
+    list1 = []
+    for angle in range(0, 10):
+        list1.append(angle)
+    for i, angle in enumerate(list1, 1):
+        print(i, angle)
+    return ret1
+
+main()
