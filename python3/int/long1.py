23e4cb6196c48a4a2094cd8968d2ae03349d1807
John Traver <john@aeropsike.com>
2020-04-23 13:13:00 -0600

    updates

--- python3/int/long1.py
+++ python3/int/long1.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-#!/usr/bin/python
+
+import sys
 
 def main():
     int1()
 
 def int1():
-    s = 9223372036854775807
-    print("%s" % str(s))
+    s = sys.maxsize
+    print("sys.maxsize = %s" % str(s))
+    # s = 9223372036854775807
+    # print("%s" % str(s))
     for _ in range(30):
         s *= 2
         print("%s" % str(s))
