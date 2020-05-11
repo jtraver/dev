56a08ab642100f203c3777540649b2276fd44536
John Traver <john@aeropsike.com>
2020-05-11 16:18:58 -0600

    commit_mac_auto_update.200511161858

---
+++ c/clock/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+
+int
+main()
+{
+    printf("main\n");
+    clock_t start, end;
+
+    /* Recording the starting clock tick.*/
+    start = clock();
+
+    // fun();
+
+    // Recording the end clock tick.
+    end = clock();
+
+    // Calculating total time taken by the program.
+    // double time_taken1 = double(end - start);
+    double time_taken1 = end - start;
+    // float time_taken1 = float(end - start);
+    // double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    printf("time taken = %g\n", time_taken1);
+    return 0;
+}
