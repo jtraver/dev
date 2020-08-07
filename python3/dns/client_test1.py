19280aa8ba603195c6ebdb7d8ce5ef87265508c3
John Traver <john@aerospike.com>
2020-08-07 15:59:06 -0400

    commit_dev_auto_update.200807155906

--- python3/dns/client_test1.py
+++ python3/dns/client_test1.py
@@ -2,8 +2,8 @@
 
 # https://www.tutorialspoint.com/python_network_programming/python_dns_look_up.htm
 
-# import dnspython as dns
-import dnspython
+import dnspython as dns
+# import dnspython
 import dns.resolver
 
 result = dns.resolver.query('tutorialspoint.com', 'A')
