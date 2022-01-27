c4d82a82b46674c1b8ddf2caafeb34b824cbe168
John Traver <john@aerospike.com>
2022-01-27 09:45:08 -0800

    commit_dev_auto_update.220127094508

--- mmix/primes/run.sh
+++ mmix/primes/run.sh
@@ -2,3 +2,5 @@ MMIX_HOME=~/mmix
 
 $MMIX_HOME/mmixal-2 $MMIX_HOME/primes.mms
 $MMIX_HOME/mmix-2 $MMIX_HOME/primes.mmo
+# $MMIX_HOME/mmmix-2 $MMIX_HOME/primes.mmconfig $MMIX_HOME/primes.mmo
+$MMIX_HOME/mmmix-2 $MMIX_HOME/primes.mmconfig $MMIX_HOME/primes.mmix
