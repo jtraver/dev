213a9f9bf4cdf7274a199e02492dd0252209ed8e
John Traver <john@aerospike.com>
2022-02-03 15:51:58 -0500

    commit_dev_auto_update.e1.220203155158

--- mmix/hello2/run.sh
+++ mmix/hello2/run.sh
@@ -5,7 +5,8 @@ MMIX_PROG=hello2
 # $MMIX_HOME/mmixal-2 --help
 # $MMIX_HOME/mmixal-2 -h
 $MMIX_HOME/mmixal-2 $MMIX_PROG.mms
-$MMIX_HOME/mmix-2 $MMIX_PROG.mmo
-$MMIX_HOME/mmix-2 -D$MMIX_PROG.mmb $MMIX_PROG.mmo
+# $MMIX_HOME/mmix-2 $MMIX_PROG.mmo
+$MMIX_HOME/mmix-2 $MMIX_PROG
+# $MMIX_HOME/mmix-2 -D$MMIX_PROG.mmb $MMIX_PROG.mmo
 # echo entery at least 405 instruction steps to run the full program
 # $MMIX_HOME/mmmix-2 $MMIX_HOME/plain.mmconfig $MMIX_PROG.mmb
