579048b21c0e5826b1ba2762d36f8258d861bef2
John Traver <john@aeropsike.com>
2022-10-06 15:30:46 -0600

    commit_dev_auto_update.mac2.221006153046

--- python3/file/file2.py
+++ python3/file/file2.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
-
 def file(filename):
     f = open(filename, "r")
     contents = ""
     for line in f:
         contents += line
     return contents
-
-
 
 # file1 = file('file1.py').read()
 file1 = file('file1.py')
