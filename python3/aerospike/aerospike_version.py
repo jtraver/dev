0de7a2210aec98386a2574e9868f03e0c4af71d8
John Traver <john@aerospike.com>
2023-03-23 15:39:33 -0400

    commit_dev_auto_update.e1.230323153933

--- python3/aerospike/aerospike_version.py
+++ python3/aerospike/aerospike_version.py
@@ -7,5 +7,11 @@ import aerospike
 def main():
     if '__version__' in dir(aerospike):
         print(("Python client version is %s" % aerospike.__version__))
+    else:
+        print("don't know what Python client version is")
+    if 'MAP_RETURN_UNORDERED_MAP' in dir(aerospike):
+        print(("Python client supports MAP_RETURN_UNORDERED_MAP (%s)" % aerospike.MAP_RETURN_UNORDERED_MAP))
+    else:
+        print("Python client does not support MAP_RETURN_UNORDERED_MAP")
 
 main()
