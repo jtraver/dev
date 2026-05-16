b08e6a1ce22035663caeec482ce29d1c6248f309
John Traver <jtraver2002@yahoo.com>
2026-05-16 13:13:33 -0600

    commit_dev_auto_update.newmac2025.260516131333

---
+++ perl/move/clean.sh
@@ -0,0 +1,58 @@
+
+mkdir save1
+mv 3grep.plx save1
+mv annotation.plx save1
+mv annotations.plx save1
+mv clean.sh save1
+mv cp1.plx save1
+mv cpajava.plx save1
+mv dontsave.plx save1
+mv fgrep.plx save1
+mv find_dirs.plx save1
+mv find_failure.plx save1
+mv find_java_signatures.plx save1
+mv get_regex.plx save1
+mv grepc.plx save1
+mv grepjs.plx save1
+mv greplua.plx save1
+mv grepphp.plx save1
+mv greppy.plx save1
+mv grepyaml.plx save1
+mv make_all.plx save1
+mv make_html.plx save1
+mv make_pic.plx save1
+mv move_all.sh save1
+mv move_gif.plx save1
+mv move_heic.plx save1
+mv move_jpeg.plx save1
+mv move_jpg.plx save1
+mv move_mov.plx save1
+mv move_mp4.plx save1
+mv move_png.plx save1
+mv move_ss.plx save1
+mv move_webp.plx save1
+mv multiline.plx save1
+mv setTextFiles.plx save1
+mv setup.sh save1
+mv show_basename.plx save1
+mv showExtension.plx save1
+mv showSize.plx save1
+mv sre.plx save1
+mv testgrep.plx save1
+
+rm -rf rmit1
+mkdir rmit1
+mv gif_html save1
+mv mp42* rmit1
+mv webp260* rmit1
+mv move2* rmit1
+mv mov2* rmit1
+mv heic2* rmit1
+mv jpg2* rmit1
+mv jpeg2* rmit1
+mv gif2* rmit1
+mv png2* rmit1
+rm -rf rmit1
+
+cp save1/* .
+
